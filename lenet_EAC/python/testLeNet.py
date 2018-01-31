24795beefe87cda1c3e18cd83cc69f7257cf6c51
Harish <harishddixit@gmail.com>
2018-02-01 02:39:47 +0530

    Added Total Black Box Control

--- lenet_EAC/python/testLeNet.py
+++ lenet_EAC/python/testLeNet.py
@@ -7,6 +7,8 @@ import cnn_lenet
 import pickle
 import copy
 import random
+import sys
+import getopt
 
 INPUT = 1
 WEIGHT = 0
@@ -157,22 +159,22 @@ def approx_gradients_step(param_grad, bits, conversion_flag):
 
     # print("Printing Original Bias")
     # print(layer1_bias)
-    param_grad[1]['b'] = approximate_and_return(layer1_bias, bits, WEIGHT)
-    param_grad[2]['b'] = approximate_and_return(layer2_bias, bits, WEIGHT)
-    param_grad[3]['b'] = approximate_and_return(layer3_bias, bits, WEIGHT)
-    param_grad[4]['b'] = approximate_and_return(layer4_bias, bits, WEIGHT)
-    param_grad[5]['b'] = approximate_and_return(layer5_bias, bits, WEIGHT)
-    param_grad[6]['b'] = approximate_and_return(layer6_bias, bits, WEIGHT)
-    param_grad[7]['b'] = approximate_and_return(layer7_bias, bits, WEIGHT)
+    param_grad[1]['b'] = approximate_and_return(layer1_bias, bits[1], WEIGHT)
+    param_grad[2]['b'] = approximate_and_return(layer2_bias, bits[2], WEIGHT)
+    param_grad[3]['b'] = approximate_and_return(layer3_bias, bits[3], WEIGHT)
+    param_grad[4]['b'] = approximate_and_return(layer4_bias, bits[4], WEIGHT)
+    param_grad[5]['b'] = approximate_and_return(layer5_bias, bits[5], WEIGHT)
+    param_grad[6]['b'] = approximate_and_return(layer6_bias, bits[6], WEIGHT)
+    param_grad[7]['b'] = approximate_and_return(layer7_bias, bits[7], WEIGHT)
 
     # bits = 9
-    param_grad[1]['w'] = approximate_and_return(layer1_weights, bits, WEIGHT)
-    param_grad[2]['w'] = approximate_and_return(layer2_weights, bits, WEIGHT)
-    param_grad[3]['w'] = approximate_and_return(layer3_weights, bits, WEIGHT)
-    param_grad[4]['w'] = approximate_and_return(layer4_weights, bits, WEIGHT)
-    param_grad[5]['w'] = approximate_and_return(layer5_weights, bits, WEIGHT)
-    param_grad[6]['w'] = approximate_and_return(layer6_weights, bits, WEIGHT)
-    param_grad[7]['w'] = approximate_and_return(layer7_weights, bits, WEIGHT)
+    param_grad[1]['w'] = approximate_and_return(layer1_weights, bits[1], WEIGHT)
+    param_grad[2]['w'] = approximate_and_return(layer2_weights, bits[2], WEIGHT)
+    param_grad[3]['w'] = approximate_and_return(layer3_weights, bits[3], WEIGHT)
+    param_grad[4]['w'] = approximate_and_return(layer4_weights, bits[4], WEIGHT)
+    param_grad[5]['w'] = approximate_and_return(layer5_weights, bits[5], WEIGHT)
+    param_grad[6]['w'] = approximate_and_return(layer6_weights, bits[6], WEIGHT)
+    param_grad[7]['w'] = approximate_and_return(layer7_weights, bits[7], WEIGHT)
     # print("Printing Approximate Bias")
     # print(layer1_bias)
 
@@ -183,20 +185,76 @@ def main():
 
   # load data
   # change the following value to true to load the entire dataset
-  bits = 10
+  bits = [ 8, 8, 8, 8, 8, 8, 8, 8]
+  max_iter = 2000
+  approximation = False
+  stepwise_print = False
 
-  fullset = False
+  # print("Reading command line arguments...")
+
+  try:
+    opts, args = getopt.getopt(sys.argv[1:],"h1:2:3:4:5:6:7:i:as")
+  except getopt.GetoptError:
+    print 'python testLeNet_quant.py -i <training iters> -a -1 <layer1 bits> -2 <layer2 bits> and so on until layer 7'
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt == '-h':
+       print '---------------------------------------------------------------------------------------------------------'
+       print 'Run Command: python testLeNet.py -a -1 <layer1 bits> -2 <layer2 bits> and so on until layer 7\n'
+       print '\t -a | --approx -> for enabling approximation (default: False)'
+       print '\t\t Use -a flag and number to specifically tune different layers for different approximations'
+       print '\t\t For the current network architecture, use approximations only for layers 1, 3, 5 and 7'
+       print '\t\t Other layer approximations do not have any effect as there are no weights for those layers (MAXPOOL/RELU etc)'
+       print '\t -s | --stepwise -> for enabling printing of test and training accuracies stepwise (default: False)'
+       print '\t -i | --iterations -> for specifying the number of training steps (default: 2000 iterations)'
+       print '\n Example Commands: '
+       print '\t python testLeNet.py -a -1 6 -3 5 -5 6 -7 6 -s -i 10000'
+       print '\t python testLeNet.py -a --l1 6 --l3 5 --l5 6 --l7 6 -s -i 10000'
+       print '\t The above commands run 10000 training iterations, with l1 @ 6 bits,'
+       print '\t l3 @ 5 bits, l5 @ 6 bits and l7 @ 6 bits with stepwise accuracy printing enabled'
+       print '---------------------------------------------------------------------------------------------------------'
+       sys.exit()
+    elif opt in ('-1', "--l1"):
+       bits[1] = int(arg)
+    elif opt in ("-2", "--l2"):
+       bits[2] = int(arg)
+    elif opt in ("-3", "--l3"):
+       bits[3] = int(arg)
+    elif opt in ("-4", "--l4"):
+       bits[4] = int(arg)
+    elif opt in ("-5", "--l5"):
+       bits[5] = int(arg)
+    elif opt in ("-6", "--l6"):
+       bits[6] = int(arg)
+    elif opt in ("-7", "--l7"):
+       bits[7] = int(arg)
+    elif opt in ("-i", "--iterations"):
+       max_iter = int(arg)
+    elif opt in ("-a", "--approx"):
+       approximation = True
+    elif opt in ("-s", "--stepwise"):
+       stepwise_print = True
+
+  print '>> Bit precision of layers [1:7] set to : ', bits
+  print '>> Training iterations : ', max_iter
+  print '>> Approximation of weights: ', approximation
+
+  # load data
+  # change the following value to true to load the entire dataset
+  fullset = True
+  # print("Loading MNIST Dataset...")
+
   print("Loading MNIST Dataset...")
   xtrain, ytrain, xval, yval, xtest, ytest = cnn_lenet.load_mnist(fullset)
-   
-  
+
   # HOOK BEGIN: Change the level of approximation here for Inputs
   # input_bits  = 2
   # xtrain = approximate_and_return(xtrain, input_bits, INPUT)
   # xval   = approximate_and_return(xval, input_bits, INPUT)
   # xtest  = approximate_and_return(xtest, input_bits, INPUT)
   # HOOK END: Change the level of approximation here for Inputs
-  
+
   #print("xvalidate approximated")
   #print(xval[:,0])
   print("MNIST Dataset Loading Complete!\n")
@@ -228,10 +286,6 @@ def main():
   param_winc = copy.deepcopy(params)
   print("Initilization Complete!\n")
 
-  print("Generating Approximation Float Table")
-  generate_float_table(bits)
-  print("Ending Approximation Float Table")
-
   for l_idx in range(1, len(layers)):
     param_winc[l_idx]['w'] = np.zeros(param_winc[l_idx]['w'].shape)
     param_winc[l_idx]['b'] = np.zeros(param_winc[l_idx]['b'].shape)
@@ -241,7 +295,11 @@ def main():
   indices = range(m_train)
   random.shuffle(indices)
 
-  print("Training Started. Printing report on training data every " + str(display_interval) + " steps.")
+  print(" ---- Training Started ---- \n")
+  if stepwise_print:
+      print("Printing report on training data every " + str(display_interval) + " steps.")
+
+
   print("Printing report on test data every " + str(test_interval) + " steps.\n")
   for step in range(max_iter):
     # get mini-batch and setup the cnn with the mini-batch
@@ -263,7 +321,8 @@ def main():
 
     # print("Printing Parameter gradient")
     # print(param_grad)
-    #approx_gradients_step(param_grad, bits, True)
+    if approximation:
+    	approx_gradients_step(param_grad, bits, True)
     # print("Ending Parameter gradient")
 
     # approx_gradients = param_grad
